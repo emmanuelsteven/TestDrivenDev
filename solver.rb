56b92ec8a76d8c89ccb57cc210995c92285cabe9
emmanuelsteven <write2stevemmy@gmail.com>
2023-07-22 17:31:32 +0100

    add solver

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
 class Solver
-    def factorial(num)
-      raise 'Number should not be negative' if num.negative?
-  
-      factorial = 1
-      i = 1
-      while i <= num
-        factorial *= i
-        i += 1
-      end
-      factorial
+  def factorial(num)
+    raise 'Number should not be negative' if num.negative?
+
+    factorial = 1
+    i = 1
+    while i <= num
+      factorial *= i
+      i += 1
     end
-  
-    def reverse(word)
-      word.reverse
-    end
-  
-    def fizzbuzz(number)
-      if (number % 3).zero? && (number % 5).zero?
-        'fizzbuzz'
-      elsif (number % 3).zero?
-        'fizz'
-      elsif (number % 5).zero?
-        'buzz'
-      else
-        number.to_s
-      end
+    factorial
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
   end
+end
